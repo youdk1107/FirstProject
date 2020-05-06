a85f1aded2c78a78b213377c58ee4ba2749bf739
dongkwan.You <youdk1107@naver.com>
2020-05-06 13:01:10 +0900

    Git 제거

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 class Test{
 	public static void main(String [] args){
-		System.out.println("Hello Git!!");
+		System.out.println("Hello!!");
 		System.out.println("Hi Git!!");
 		System.out.println("branch1");
 		System.out.println("branch2");
