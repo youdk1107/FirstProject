069b6674b50a5d1467d23e77c0f31dc3e76ffac3
dongkwan.You <youdk1107@naver.com>
2020-05-06 12:37:18 +0900

    branch1에 추가

--- Test.java
+++ Test.java
@@ -1,6 +1,7 @@
 class Test{
 	public static void main(String [] args){
 		System.out.println("Hello Git!!");
-		System.out.println("Hello Git!!");
+		System.out.println("Hi Git!!");
+		System.out.println("branch1");
 	}
 }
